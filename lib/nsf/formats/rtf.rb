6b020ce16f97a659bcd4b631cbd0dfcb643bed47
Brenton 'B-Train' Fletcher <i@bloople.net>
2011-03-23 00:42:09 +1100

    WIP RTF import

--- lib/nsf/formats/rtf.rb
+++ lib/nsf/formats/rtf.rb
@@ -1,9 +1,36 @@
 require 'rtf'
+require 'ruby-rtf'
 
 module Nsf
   class Document
+    def self.from_rtf(text)
+      nodes = []
+      current_text = ""
+
+      (RubyRTF::Parser.new.parse(text).sections + [{ :text => '', :paragraph => true, :modifiers => {} }]).each do |sec|
+        puts sec.inspect
+        new_text = sec[:text]
+
+        unless new_text.gsub(/[[:space:]]+/, ' ').blank?
+          new_text = "*#{new_text}*" if sec[:modifiers][:bold]
+          new_text = "_#{new_text}_" if sec[:modifiers][:italic]
+        end
+
+        current_text << new_text
+
+        if sec[:modifiers][:paragraph]
+          paragraph_text = current_text.gsub(/[[:space:]]+/, ' ').strip
+          nodes << Paragraph.new(paragraph_text) if paragraph_text.present?
+          current_text = ""
+        end
+      end
+
+      Document.new(nodes)
+    end
+
     def to_rtf
       doc = RTF::Document.new(RTF::Font.new(RTF::Font::ROMAN, 'Times New Roman'))
+
       nodes.each do |node|
         doc.paragraph << node.to_rtf
       end
@@ -13,20 +40,30 @@ module Nsf
   end
      
   class Paragraph
+    #RTF_BOLD = RTF::CharacterStyle.new
+    #RTF_BOLD.bold = true
+
+    #RTF_ITALIC = RTF::CharacterStyle.new
+    #RTF_ITALIC.italic = true
+
     def to_rtf
+      #cn = RTF::CommandNode.new('', '')
+      #cn.paragraph do |p|
+      #  elements = @text.split(/[\*_]/)
+      #end
       @text.gsub("\n", " ")
     end
   end
 
   class Fixedblock < Paragraph
     def to_rtf
-      @text
+      @text.gsub("\n", " ")
     end
   end
 
   class Heading
     def to_rtf
-      @text
+      @text.gsub("\n", " ")
     end
   end
 end
