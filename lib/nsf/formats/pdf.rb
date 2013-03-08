e2a08eec0338805a2d86c12e7428033327a7adda
Brenton "B-Train" Fletcher <i@bloople.net>
2013-03-08 11:51:37 +1100

    Make everything be based off of the base font size, and allow setting the base font size when you call Document#to_pdf(font_size = ...); the base font size defaults to 10.5pt now, as 14pt was a bit too big for A4; remove no longer required pdf_pt method

--- lib/nsf/formats/pdf.rb
+++ lib/nsf/formats/pdf.rb
@@ -4,23 +4,17 @@ require 'prawn'
 
 module Nsf
   class Document
-    def self.pdf_pt(px)
-      #Assumes screen has true dpi of 96
-      (px * (1 / 96.0)) / (1 / 72.0)
-    end
-
-    PDF_MARGINS = pdf_pt(40)
-    PDF_FONT_SIZE = pdf_pt(18)
+    PDF_DEFAULT_FONT_SIZE = 10.5
     PDF_LEADING = 0.4
-    def to_pdf
-      pdf = Prawn::Document.new(:page_size => "A4", :margin => PDF_MARGINS)
+    def to_pdf(base_font_size = PDF_DEFAULT_FONT_SIZE)
+      pdf = Prawn::Document.new(:page_size => "A4", :margin => (base_font_size * 2.22222).round)
       pdf.font_families.update("Open Sans" => {
         :normal => "#{File.dirname(__FILE__)}/fonts/OpenSans-Regular.ttf",
         :bold => "#{File.dirname(__FILE__)}/fonts/OpenSans-Bold.ttf"
       })
       pdf.font "Open Sans"
-      pdf.font_size = PDF_FONT_SIZE
-      pdf.default_leading = PDF_LEADING * PDF_FONT_SIZE
+      pdf.font_size = base_font_size
+      pdf.default_leading = (PDF_LEADING * base_font_size).round
 
       nodes.each { |n| n.to_pdf(pdf) }
 
@@ -29,20 +23,20 @@ module Nsf
   end
 
   class Paragraph
-    PDF_PARAGRAPH_LEADING = 1.0
+    PDF_PARAGRAPH_LEADING = 1.2
     def to_pdf(pdf)
       pdf.text to_html_fragment(false), :inline_format => true
-      pdf.move_down Document::PDF_FONT_SIZE * PDF_PARAGRAPH_LEADING
+      pdf.move_down (pdf.font_size * PDF_PARAGRAPH_LEADING).round
     end
   end
 
   class Heading
-    PDF_FONT_SIZES = [40, 30, 24].map { |px| Document.pdf_pt(px) }
+    PDF_FONT_RATIOS = [2.22222, 1.66666, 1.33333]
     PDF_HEADING_LEADING = 0.6
     def to_pdf(pdf)
-      size = PDF_FONT_SIZES[level - 1]
-      pdf.text text, :size => size, :leading => size * Document::PDF_LEADING, :style => :bold
-      pdf.move_down size * PDF_HEADING_LEADING
+      size = (PDF_FONT_RATIOS[level - 1] * pdf.font_size).round
+      pdf.text text, :size => size, :leading => (size * Document::PDF_LEADING).round, :style => :bold
+      pdf.move_down (size * PDF_HEADING_LEADING).round
     end
   end
 end
